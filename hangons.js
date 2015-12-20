b9937e30d33d165e12dd7aae4b668ecca36b5ac1
David Byrne <David.dbyrne@gmail.com>
2015-12-20 13:56:55 +0000

    Adds support for sender name and sent time

--- hangons.js
+++ hangons.js
@@ -20,50 +20,87 @@ function readFile(evt) {
             //console.log("Name: "+files[0].name);
 			//console.log("Size: "+files[0].size+" Bytes");
 			parseData();
-			//console.dir(jsonData.conversation_state[0].conversation_state.event[23].chat_message.message_content.segment[0].text);
 		}
 	}
 	else 
     {
-	      console.warn("Failed to load files"); 
+	      console.error("Failed to load files"); 
     }
 }
 
 function parseData()
 {
-    console.dir(jsonData);
     var i = 0;
     for (; i < jsonData.conversation_state.length; i++)
     {
+        var participants = getParticipants(i);
         var j = 0;
         for(; j < jsonData.conversation_state[i].conversation_state.event.length; j++)
         {
-            //console.log(j);
+            console.log("Sender:"+ getName(jsonData.conversation_state[i].conversation_state.event[j].sender_id.gaia_id, participants))
+            var d = new Date(0); //0 means it sets the date to the epoch
+            d.setUTCSeconds(Math.floor(jsonData.conversation_state[i].conversation_state.event[j].timestamp/1000000));//convert from microseconds to seconds
+            console.log("At:"+d);
+
             if (jsonData.conversation_state[i].conversation_state.event[j].chat_message.message_content.segment !== undefined)
             {//if it's a normal hangouts message
-                var k = 0;
-                for (; k < jsonData.conversation_state[i].conversation_state.event[j].chat_message.message_content.segment.length; k++)
+                
+                for (var k = 0; k < jsonData.conversation_state[i].conversation_state.event[j].chat_message.message_content.segment.length; k++)
                 {
-                    console.log(jsonData.conversation_state[i].conversation_state.event[j].chat_message.message_content.segment[k].text);
+                    console.log("Message: "+jsonData.conversation_state[i].conversation_state.event[j].chat_message.message_content.segment[k].text);
                 }
             }
             
             
             else if (jsonData.conversation_state[i].conversation_state.event[j].chat_message.message_content.attachment !== undefined)
-            {//if it's an message
-                var k = 0;
-                for (; k < jsonData.conversation_state[i].conversation_state.event[j].chat_message.message_content.attachment.length; k++)
+            {//if it's an image
+                
+                for (var k = 0; k < jsonData.conversation_state[i].conversation_state.event[j].chat_message.message_content.attachment.length; k++)
                 {
-                    console.log(jsonData.conversation_state[i].conversation_state.event[j].chat_message.message_content.attachment[k].embed_item["embeds.PlusPhoto.plus_photo"].url);
+                    console.log("Image: "+jsonData.conversation_state[i].conversation_state.event[j].chat_message.message_content.attachment[k].embed_item["embeds.PlusPhoto.plus_photo"].url);
                 }
             }
             
             else
             {
-                console.error("%c Unknown format for conversation "+i+" message "+j+"", "background: #FF0000");
+                console.warn("%c Unknown format for conversation "+i+" message "+j+"", "background: #FF0000");
             }
         }
     }
+}
+
+function getParticipants(index)
+{
+    var participants = [];
+    for (var i = 0; i < jsonData.conversation_state[index].conversation_state.conversation.participant_data.length; i++)
+    {
+        var person = {};
+        person.id = jsonData.conversation_state[index].conversation_state.conversation.participant_data[i].id.gaia_id;
+        if (jsonData.conversation_state[index].conversation_state.conversation.participant_data[i].fallback_name !== undefined)
+        {
+            person.name = jsonData.conversation_state[index].conversation_state.conversation.participant_data[i].fallback_name;
+        }
+        else 
+        {
+            person.name = jsonData.conversation_state[index].conversation_state.conversation.participant_data[i].id.gaia_id;
+        }
+        participants.push(person);
+    }
+    return participants;
+}
+
+function getName(id, participants)
+{
+    for (var i = 0; i < participants.length; i++)
+    {
+        if(id === participants[i].id)
+        {
+            return participants[i].name;
+        }
+    }
+    console.warn("Name not found for "+id+" in");
+    console.dir(participants);
+    return id;
 }
 
 function download(filename, text) {
