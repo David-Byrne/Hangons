399eb346565da3119a6075d82d9cff0667bd1c11
TabulateJarl8 <tabulatejarl8@gmail.com>
2021-03-06 15:23:54 -0500

    fix linkify regular expressions

--- hangons.js
+++ hangons.js
@@ -230,12 +230,12 @@ function linkify(inputText)
     }
 
     //URLs starting with http://, https://, or ftp:// that aren't preceded by src=" (images)
-    replacePattern1 = /((?!src=").{5}|^.{0,4})(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+    replacePattern1 = /(?:(?!src=").{5}|^.{0,4})(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
     replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
 
     //URLs starting with "www." (without // before it, or it'd re-link the ones done above) that aren't preceded by src=" (images)
-    replacePattern2 = /((?!src=").{5}|^.{0,4})(^|[^\/])(www\.[\S]+(\b|$))/gim;
-    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+    replacePattern2 = /(?:(?!src=").{5}|^.{0,4})((^|[^\/])(www\.[\S]+(\b|$)))/gim;
+    replacedText = replacedText.replace(replacePattern2, '<a href="http://$1" target="_blank">$1</a>');
 
     //Change email addresses to mailto:: links.
     replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
